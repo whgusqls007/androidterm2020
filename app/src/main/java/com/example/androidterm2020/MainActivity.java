213926d40f303da187bdbbe12cb5aea121713c3b
WooSeongChoi <48307285+WooSeongChoi@users.noreply.github.com>
2020-05-01 21:49:19 +0900

    2020/05/01
    화면전환 기능추가

--- app/src/main/java/com/example/androidterm2020/MainActivity.java
+++ app/src/main/java/com/example/androidterm2020/MainActivity.java
@@ -4,7 +4,10 @@ package com.example.androidterm2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        //201524517
+
+        Button button = (Button)findViewById(R.id.nextActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ScheduleRegistrationActivity.class);
+                intent.putExtra("message", "반갑습니다.");
+                startActivity(intent);
+            }
+        });
     }
 }
 //마지막 커밋 테스트 입니다.
