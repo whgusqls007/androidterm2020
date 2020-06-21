46bf1c0700ee708b4de85bd526196b1693e34670
Yoon Tae Wan <yun03083@gmail.com>
2020-06-22 00:24:46 +0900

    0622

--- app/src/main/java/com/example/androidterm2020/SettingPage.java
+++ app/src/main/java/com/example/androidterm2020/SettingPage.java
@@ -34,33 +34,26 @@ public class SettingPage extends PreferenceFragment {
         prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
         if(prefs.getBoolean("notify_on_off", true)){
             NotifyView.setSummary("사용");
-            SettingInfo.Noti_on_off = true;
         }
 
         if(prefs.getString("notify_select", "All").equals("All")){
             NotifySelct.setSummary(prefs.getString("notify_select", "All"));
-            SettingInfo.Noti_kind = 3;
         }
         else if(prefs.getString("notify_select", "일정").equals("일정")){
             NotifySelct.setSummary(prefs.getString("notify_select", "일정"));
-            SettingInfo.Noti_kind = 1;
         }
         else if(prefs.getString("notify_select", "기상 정보").equals("기상 정보")){
             NotifySelct.setSummary(prefs.getString("notify_select", "기상 정보"));
-            SettingInfo.Noti_kind = 2;
         }
 
         if(prefs.getString("notify_time", "1시간 전").equals("1시간 전")){
             NotifyTime.setSummary(prefs.getString("notify_time", "1시간 전"));
-            SettingInfo.Noti_time = 1;
         }
         else if(prefs.getString("notify_time", "2시간 전").equals("2시간 전")){
             NotifyTime.setSummary(prefs.getString("notify_time", "2시간 전"));
-            SettingInfo.Noti_time = 2;
         }
         else if(prefs.getString("notify_time", "3시간 전").equals("3시간 전")){
             NotifyTime.setSummary(prefs.getString("notify_time", "3시간 전"));
-            SettingInfo.Noti_time = 3;
         }
 
         prefs.registerOnSharedPreferenceChangeListener(prefListener);
@@ -73,12 +66,10 @@ public class SettingPage extends PreferenceFragment {
                 if(prefs.getBoolean("notify_on_off", false)){
                     NotifyView.setSummary("사용");
                     NotifyView.setDefaultValue(true);
-                    SettingInfo.Noti_on_off = true;
                 }
                 else{
                     NotifyView.setSummary("사용 안함");
                     NotifyView.setDefaultValue(false);
-                    SettingInfo.Noti_on_off = false;
                 }
                 ((BaseAdapter)getPreferenceScreen().getRootAdapter()).notifyDataSetChanged();
             }
@@ -87,17 +78,14 @@ public class SettingPage extends PreferenceFragment {
                 if(prefs.getString("notify_select", "All").equals("All")){
                     NotifySelct.setSummary("All");
                     NotifySelct.setDefaultValue("All");
-                    SettingInfo.Noti_kind = 3;
                 }
                 else if(prefs.getString("notify_select", "일정").equals("일정")){
                     NotifySelct.setSummary("일정");
                     NotifySelct.setDefaultValue("일정");
-                    SettingInfo.Noti_kind = 1;
                 }
                 else{
                     NotifySelct.setSummary("기상 정보");
                     NotifySelct.setDefaultValue("기상 정보");
-                    SettingInfo.Noti_kind = 2;
                 }
                 ((BaseAdapter)getPreferenceScreen().getRootAdapter()).notifyDataSetChanged();
             }
@@ -106,17 +94,14 @@ public class SettingPage extends PreferenceFragment {
                 if(prefs.getString("notify_time", "1시간 전").equals("1시간 전")){
                     NotifyTime.setSummary("1시간 전");
                     NotifyTime.setDefaultValue("1시간 전");
-                    SettingInfo.Noti_time = 1;
                 }
                 else if(prefs.getString("notify_time", "2시간 전").equals("2시간 전")){
                     NotifyTime.setSummary("2시간 전");
                     NotifyTime.setDefaultValue("2시간 전");
-                    SettingInfo.Noti_time = 2;
                 }
                 else{
                     NotifyTime.setSummary("3시간 전");
                     NotifyTime.setDefaultValue("3시간 전");
-                    SettingInfo.Noti_time = 3;
                 }
                 ((BaseAdapter)getPreferenceScreen().getRootAdapter()).notifyDataSetChanged();
             }
