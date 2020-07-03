1f2d01911e1ee564d767b8b6c31db4bc7810646b
WooSeongChoi <48307285+WooSeongChoi@users.noreply.github.com>
2020-07-03 11:55:22 +0900

    0703 Alarm에 foreign key 추가
    Alarm에 foreign key 추가해서 Schedule을 지울때 알람도 같이 지울수 있게 하려고 한다.

--- app/src/main/java/com/example/androidterm2020/RoomDB/Alarm.java
+++ app/src/main/java/com/example/androidterm2020/RoomDB/Alarm.java
@@ -3,16 +3,23 @@ package com.example.androidterm2020.RoomDB;
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "alarm_tb")
+@Entity(tableName = "alarm_tb", foreignKeys = @ForeignKey(entity = Schedule.class,
+        parentColumns = "sid",
+        childColumns = "scheduleId"))
 public class Alarm {
     @PrimaryKey(autoGenerate = true)
     @NonNull
     @ColumnInfo(name = "aid")
     private int aid;
 
-    private int requestId;
+    private final int requestId;
+
+
+    @ColumnInfo(name = "scheduleId")
+    private final int scheduleId;
 
     public int getAid() {
         return aid;
@@ -22,15 +29,16 @@ public class Alarm {
         this.aid = aid;
     }
 
+    public int getScheduleId() {
+        return scheduleId;
+    }
+
     public int getRequestId() {
         return requestId;
     }
 
-    public void setRequestId(int requestId) {
+    public Alarm(int requestId, int scheduleId) {
         this.requestId = requestId;
-    }
-
-    public Alarm(int requestId) {
-        this.requestId = requestId;
+        this.scheduleId = scheduleId;
     }
 }
