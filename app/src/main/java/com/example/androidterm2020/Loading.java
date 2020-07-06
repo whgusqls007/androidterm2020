b45c226afa9960f817bcecb096a3215290239545
Yoon Tae Wan <yun03083@gmail.com>
2020-07-06 22:18:48 +0900

    Loading 추가

---
+++ app/src/main/java/com/example/androidterm2020/Loading.java
@@ -0,0 +1,31 @@
+package com.example.androidterm2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class Loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        ImageView view = (ImageView)findViewById(R.id.imageView);
+        AnimationDrawable frameAnimation = (AnimationDrawable)view.getBackground();
+        frameAnimation.start();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
